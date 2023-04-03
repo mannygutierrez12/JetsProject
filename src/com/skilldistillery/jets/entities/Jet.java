cb3d054077d21efc4594190a1b1d328fedab9352
mannygutierrez12 <35671250+mannygutierrez12@users.noreply.github.com>
2023-04-02 21:28:09 -0400

    Created Jet Class

--- src/com/skilldistillery/jets/entities/Jet.java
+++ src/com/skilldistillery/jets/entities/Jet.java
@@ -1,5 +1,61 @@
 package com.skilldistillery.jets.entities;
 
 public abstract class Jet {
+	private String Type;
 	private String model;
+	private double speed;
+	private double range;
+	private double price;
+	
+	public abstract void fly();
+	
+	public abstract void takeOff();
+	
+	public abstract void land();
+	
+	public abstract void communicateWithControlTower();
+	
+	public abstract void fuelingTank();
+
+	public String getType() {
+		return Type;
+	}
+
+	public void setType(String type) {
+		Type = type;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public double getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
+	public double getRange() {
+		return range;
+	}
+
+	public void setRange(double range) {
+		this.range = range;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
 }
