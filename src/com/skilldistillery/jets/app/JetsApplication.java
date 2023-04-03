11d64d23fd99bf702d9ddb8d899f7e3580af0292
mannygutierrez12 <35671250+mannygutierrez12@users.noreply.github.com>
2023-04-03 01:53:22 -0400

    Upload new changes

--- src/com/skilldistillery/jets/app/JetsApplication.java
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -1,12 +1,94 @@
 package com.skilldistillery.jets.app;
 
+import java.util.List;
+import java.util.Scanner;
+
+import com.skilldistillery.jets.entities.Airfield;
+import com.skilldistillery.jets.entities.Jet;
+
 public class JetsApplication {
+	
+	private Airfield airfield = new Airfield();
+	private Scanner input = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JetsApplication app = new JetsApplication();
+		app.launch();
 	}
 
-} public void launch() {
 	
+
+	private void launch() {
+		// airfield.populateAirfieldFromFile("jets.txt");
+		
+		
+		boolean amIDone = true;
+		while (amIDone) {
+			mainMenu();
+			String choice = input.nextLine();
+
+			if (choice.equals("1")) {
+				listFleet();
+			} else if (choice.equals("2")) {
+				flyAllJets();
+			} else if (choice.equals("3")) {
+				viewFastestJet();
+//	         } else if (choice.equals("4")) {
+//	             viewJetWithLongestRange();
+//	         } else if (choice.equals("5")) {
+//	             loadAllCargoJets();
+//	         } else if (choice.equals("6")) {
+//	             dogfight();
+//	         } else if (choice.equals("7")) {
+//	             addJetToFleet();
+//	         } else if (choice.equals("8")) {
+//	             removeJetFromFleet();
+	         } else if (choice.equals("9")) {
+	        	 amIDone = false;
+	        	 System.out.println("See you later");
+	        	 break;
+			} else {
+				System.out.println("Invalid input. Please try again.");
+			}
+		}
+	}
+
+	private void mainMenu() {
+		System.out.println();
+		System.out.println("PLEASE CHOOSE AN ITEM FROM THE MENU BELOW");
+		System.out.println("1. List fleet");
+		System.out.println("2. Fly all jets");
+		System.out.println("3. View fastest jet");
+		System.out.println("4. View jet with longest range");
+		System.out.println("5. Load all Cargo Jets");
+		System.out.println("6. Dogfight!");
+		System.out.println("7. Add a jet to Fleet");
+		System.out.println("8. Remove a jet from Fleet");
+		System.out.println("9. Quit");
+		System.out.print("PLEASE ENTER A NUMBER FROM THE MENU ABOVE: ");
+		System.out.println();
+		
+	}
+
+	public void listFleet() {
+		airfield.printFleetDetails();
+	}
+	
+
+	public void flyAllJets() {
+	    airfield.flyAllJets();
+	}
+
+	private void viewFastestJet() {
+		List<Jet> jets = airfield.getFleet();
+		Jet fastestJet = jets.get(0);
+		for (int i = 1; i < jets.size(); i++) {
+			Jet jet = jets.get(i);
+			if (jet.getSpeed() > fastestJet.getSpeed()) {
+				fastestJet = jet;
+			}
+		}
+		System.out.println("Fastest jet: " + fastestJet.toString());
+	}
+
 }
--- src/com/skilldistillery/jets/entities/Airfield.java
+++ src/com/skilldistillery/jets/entities/Airfield.java
@@ -7,59 +7,66 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Airfield {
-	
-	private List<Jet> fleet = new ArrayList<>();
-	
+	private List<Jet> fleet;
+
 	public Airfield() {
-		
+		fleet = readFromFile();
 	}
-	
+
+	public void setFleet(List<Jet> fleet) {
+		this.fleet = fleet;
+	}
+
+	public void printFleetDetails() {
+		for (Jet jet : fleet) {
+			System.out.println(jet.getModel() + ", speed: " + jet.getSpeed()
+					+ ", range: " + jet.getRange() + ", price: " + jet.getPrice());
+		}
+	}
+
 	public List<Jet> getFleet() {
 		return fleet;
 	}
-
-	public void addNewJet(Jet jet) {
-		fleet.add(jet);
-	}
 	
-	public void removeJet(int index) {
-		fleet.remove(index);
+	public void flyAllJets() {
+		for (Jet jet : fleet) {
+		    System.out.println(jet.getModel() + " is flying for " + (int)(jet.getRange() / jet.getSpeed()) + " hours.");
+		    jet.fly();
+		}
+		
 	}
 
-	public List<Jet> readFromFile(String fn) {
+	
+
+
+	public List<Jet> readFromFile() {
 		List<Jet> jets = new ArrayList<>();
-		
+
 		try (BufferedReader bufIn = new BufferedReader(new FileReader("jets.txt"))) {
 			String aJet;
 			while ((aJet = bufIn.readLine()) != null) {
 				String[] jetDetails = aJet.split(",");
-				
-				// create the appropriate jet based on details
 				String type = jetDetails[0];
 				String model = jetDetails[1];
 				double speed = Double.parseDouble(jetDetails[2]);
 				int range = Integer.parseInt(jetDetails[3]);
-				long price = Long.parseLong(jetDetails[4].replaceAll("_", ""));
-				
+				long price = Long.parseLong(jetDetails[4]);
+
 				Jet jet = null;
-				
 				if (type.equals("Cargo")) {
 					jet = new CargoJet(model, speed, range, price);
-				}
-				else if (type.equals("Fighter")) {
+				} else if (type.equals("Fighter")) {
 					jet = new CombatJet(model, speed, range, price);
-				}
-				else if (type.equals("Passenger")) {
+				} else if (type.equals("Passenger")) {
 					jet = new PassengerJet(model, speed, range, price);
 				}
-				
 				jets.add(jet);
 			}
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			System.err.println(e);
 		}
-		
+
 		return jets;
 	}
+
 }
